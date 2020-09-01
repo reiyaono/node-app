e48384e3cb316b3f199e0554ba8b9351a178d4ca
reiya ono <reiya.ono@linkbal.co.jp>
2020-09-01 16:48:24 +0900

    6-3

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -1,9 +1,88 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db =  require('../models/index')
+const { Op } = require('sequelize')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  db.User.findAll().then(users => {
+    var data = {
+      title: 'User/Index',
+      content: users
+    }
+    res.render('users/index', data)
+  })
+});
+// router.get('/', (req, res, next) => {
+//   const id = req.query.id
+//   db.User.findAll(
+//     { where: {
+//       id: { [Op.lte]:id }
+//     } }
+//   ).then(users => {
+//     var data = {
+//       title: 'User/Index',
+//       content: users
+//     }
+//     res.render('users/index', data)
+//   })
+// });
+// router.get('/', (req, res, next) => {
+//   const id = req.query.id
+//   const name = req.query.name
+//   db.User.findAll(
+//     { where: {
+//       name: { [Op.like]: '%' + name + '%' }
+//     } }
+//   ).then(users => {
+//     var data = {
+//       title: 'User/Index',
+//       content: users
+//     }
+//     res.render('users/index', data)
+//   })
+// });
+// router.get('/', (req, res, next) => {
+//   const id = req.query.id
+//   const name = req.query.name
+//   const mail = req.query.mail
+//   const min = req.query.min * 1
+//   const max = req.query.max * 1
+//   db.User.findAll(
+//     { where: {
+//       // age: { [Op.gte]: min, [Op.lte]: max }
+//       [Op.or]: [
+//         { name: { [Op.like]: '%' + name + '%' } },
+//         { mail: { [Op.like]: '%' + mail + '%' } }
+//       ]
+//     } }
+//   ).then(users => {
+//     var data = {
+//       title: 'User/Index',
+//       content: users
+//     }
+//     res.render('users/index', data)
+//   })
+// });
+
+router.get('/add', (req, res, next) => {
+  const data = {
+    title: 'User/add'
+  }
+  res.render('users/add', data)
+});
+
+router.post('/add', (req, res, next) => {
+  db.sequelize.sync()
+    .then(() => db.User.create({
+      name: req.body.name,
+      pass: req.body.pass,
+      mail: req.body.mail,
+      age: req.body.age
+    }))
+    .then(usr => {
+      res.redirect('/users')
+    })
 });
 
 module.exports = router;

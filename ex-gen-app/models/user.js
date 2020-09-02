c45e2689780ec8f7cdc0ba4e1bd8252fe3c1e62f
reiya ono <reiya.ono@linkbal.co.jp>
2020-09-02 16:12:23 +0900

    6-4

--- ex-gen-app/models/user.js
+++ ex-gen-app/models/user.js
@@ -3,24 +3,45 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+
+  const User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "名前は必ず入力してください。"
+        }
+      }
+    },
+    pass: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "パスワードは必ず入力してください。"
+        }
+      }
+    },
+    mail: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: "メールアドレスを入力してください。"
+        }
+      }
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: { msg: "整数を入力してください "},
+        min: {
+          arg: [0],
+          msg: "ゼロ以上の値が必要です。"
+        }
+      }
     }
-  };
-  User.init({
-    name: DataTypes.STRING,
-    pass: DataTypes.STRING,
-    mail: DataTypes.STRING,
-    age: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  }, {})
+  User.associate = function(models) {
+
+  }
   return User;
 };
--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -67,22 +67,75 @@ router.get('/', (req, res, next) => {
 
 router.get('/add', (req, res, next) => {
   const data = {
-    title: 'User/add'
+    title: 'User/add',
+    form: new db.User(),
+    err: null
   }
   res.render('users/add', data)
 });
 
 router.post('/add', (req, res, next) => {
+  const form = {
+    name: req.body.name,
+    pass: req.body.pass,
+    mail: req.body.mail,
+    age: req.body.age
+  }
   db.sequelize.sync()
-    .then(() => db.User.create({
-      name: req.body.name,
-      pass: req.body.pass,
-      mail: req.body.mail,
-      age: req.body.age
-    }))
+    .then(() => db.User.create(form))
     .then(usr => {
       res.redirect('/users')
     })
+    .catch(err => {
+      const data = {
+        title: 'User/Add',
+        form: form,
+        err: err
+      }
+      res.render('users/add', data)
+    })
 });
 
+router.get('/edit', (req, res, next) => {
+  db.User.findByPk(req.query.id)
+    .then(usr => {
+      const data = {
+        title: 'Users/Edit',
+        form: usr
+      }
+      res.render('users/edit', data)
+    })
+});
+
+router.post('/edit', (req, res, next) => {
+  db.User.findByPk(req.query.id)
+    .then(usr => {
+      usr.name = req.body.name,
+      usr.pass = req.body.pass,
+      usr.mail = req.body.mail,
+      usr.age = req.body.age
+      usr.save().then(() => res.redirect('/users'))
+    })
+});
+
+router.get('/delete', (req, res, next) => {
+  db.User.findByPk(req.query.id)
+    .then(usr => {
+      var data = {
+        title: 'Users/Delete',
+        form: usr
+      }
+      res.render('users/delete', data)
+    })
+})
+
+router.post('/delete', (req, res, next) => {
+  db.sequelize.sync()
+  .then(() => db.User.destroy({
+    where: {id: req.body.id }
+  }))
+  .then(usr => {
+    res.redirect('/users')
+  })
+})
 module.exports = router;
